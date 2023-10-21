0aad6f5d54f1284d59d3151baeba6575e68a9c1d
Obi Tam <omieteobi@gmail.com>
2023-10-21 02:25:44 +0100

    added the auth component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 // src/App.js
-import React from 'react';
+import React, { useState } from 'react';
 import MainForm from './components/MainForm';
 import styles from './styles.module.css'
+import AuthForm from './components/AuthForm';
 
 function App() {
+    const [loggedIn, setLoggedIn] = useState(false)
     return (
         <div className={styles['app']}>
-            <MainForm />
+            <h1>
+                Welcome to my Form Project!
+            </h1>
+            {
+                loggedIn ?
+                    <MainForm logout={() => setLoggedIn(false)} />
+                    :
+                    <AuthForm login={() => setLoggedIn(true)} />
+            }
         </div>
     )
 }
