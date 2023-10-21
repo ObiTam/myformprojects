1117633ee6ef0fc0c696a3c69844a17864c7d1c9
Obi Tam <omieteobi@gmail.com>
2023-10-21 01:11:33 +0100

    completed styling form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // src/App.js
 import React from 'react';
 import MainForm from './components/MainForm';
+import styles from './styles.module.css'
 
 function App() {
     return (
-        <div>
+        <div className={styles['app']}>
             <MainForm />
         </div>
-    );
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(<App />);
+
